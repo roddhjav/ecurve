8feb216a8cc42c77248b492601537436f87aedd0
Alexandre PUJOL <alexandre.pujol.1@etu.univ-amu.fr>
2014-11-18 23:02:39 +0100

    Add ideal file

--- point.py
+++ point.py
@@ -135,30 +135,4 @@ class Point(object):
 
    def __getitem__(self, index):
       return [self.x, self.y][index]
-   
-
-
-class Ideal(Point):
-   def __init__(self, curve):
-      self.curve = curve
-
-   def __neg__(self):
-      return self
-
-   def __str__(self):
-      return "Ideal"
-
-   def __add__(self, Q):
-      if self.curve != Q.curve:
-         raise Exception("Can't add points on different curves!")
-      return Q
-
-   def __mul__(self, n):
-      if not isinstance(n, int):
-         raise Exception("Can't scale a point by something which isn't an int!")
-      else:
-         return self
-
-   def __eq__(self, other):
-      return type(other) is Ideal
 
