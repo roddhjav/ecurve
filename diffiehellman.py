9e095ec0693aa9d6cdbb33d044b5c3a601b6ca74
Alexandre PUJOL <alexandre.pujol.1@etu.univ-amu.fr>
2014-11-26 14:27:19 +0100

    DH tested

--- diffiehellman.py
+++ diffiehellman.py
@@ -3,14 +3,19 @@ import random
 from elliptic import *
 from point import *
 
+# TODO : Add comment about var
 class DH(object):
    def __init__(self, curve):
       self.curve = curve
-      self.a = random.getrandbits(246)
-      print self.curve.n
-      print self.a
-      assert self.a < self.curve.n, "bla"
+      self.a = random.getrandbits(246) # See if it is a secure random
+      assert self.a < self.curve.n, "Error"
       
-
-   #def keychange(self):
-
+      self.g = Point(self.curve, self.curve.gx,  self.curve.gy)
+      self.ga = self.a*self.g
+      
+      print("a   =", self.a)
+      print("g   =", self.g)
+      print("g^a =", self.ga)
+      
+   def setkey(self, gb):
+      self.gba = self.a*gb
