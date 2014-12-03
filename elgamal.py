d97e6ae33708175c478e87ba4c73553da3594eba
Alexandre PUJOL <alexandre.pujol.1@etu.univ-amu.fr>
2014-12-03 22:04:08 +0100

    Elgamal : Add comments

--- elgamal.py
+++ elgamal.py
@@ -5,19 +5,31 @@ from point import *
 
 class Elgamal(object):
    """ ElGamal encryption
-   - curve EllipticCurve
-   - generator Point
+   - self.curve (EllipticCurve) The elliptic curve used
+   - self.generator (Point) A generator of the curve
    """
    def __init__(self, curve):
       self.curve = curve
       self.generator = Point(self.curve, self.curve.gx, self.curve.gy)
-      
+   
+   """ Generate elgamal keys
+    - publickey (Point) the public key
+    - privatekey (int) the private key
+   """
    def keygen(self):
       bits = int(math.log(self.curve.n, 2))
       privatekey = random.getrandbits(bits - 1)
       publickey = privatekey*self.generator
       return (publickey, privatekey)
       
+   """ Crypt a message
+    Input :
+    - publickey (Point) the public key
+    - m (int) message
+    Output :
+    - c1 (int)
+    - c2 (Point)
+   """
    def crypt(self, publickey, m):
       bits = int(math.log(self.curve.n, 2))
       k = random.getrandbits(bits - 1)
@@ -25,5 +37,14 @@ class Elgamal(object):
       c2 = k*self.generator
       return (c1, c2)
       
+   """ Decrypt a message
+    Input :
+    - privatekey (int) the private key
+    - c1 (int)
+    - c2 (Point)
+    output :
+    - Message decrypted (int)
+   """
    def decrypt(self, privatekey, c1, c2):
       return c1 - (privatekey*c2).x
+      
