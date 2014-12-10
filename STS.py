10d2549faed8f8edb877a87e505482f3dc609aa4
Alexandre PUJOL <alexandre.pujol.1@etu.univ-amu.fr>
2014-12-11 00:59:39 +0100

    STS : Add verifysecret

--- STS.py
+++ STS.py
@@ -77,19 +77,33 @@ class STS(object):
       # Encoding
       encrypted = base64.b64encode(encrypted)
       iv = base64.b64encode(iv)
+      print("(r,s)  : " + str(r) + "," + str(s))
       print("Ek(Sb) : " + str(encrypted))
       print("iv     : " + str(iv))
       print("key    : " + str(base64.b64encode(aes_key)))
 
       return (K, encrypted, iv)
 
-#   def verifysecret(self, ecdsa_publickey, x, gx, gy, encrypted, iv):
-#      K = x*gy
-#      
-#      cipher = AES.new(aes_key, AES.MODE_CBC, IV)
-#      signed = cipher.decrypt(encoded)
+   def verifysecret(self, ecdsa_publickey, x, gx, gy, encrypted, iv):
+      K = x*gy
+      aes_key = bin(K.x)
+      aes_key = hashlib.sha256(aes_key.encode()).digest()
+      
+      iv = base64.b64decode(iv)
+      encrypted = base64.b64decode(encrypted)
+      
+      cipher = AES.new(aes_key, AES.MODE_CBC, iv)
+      signed = cipher.decrypt(encrypted)
 
-#      (x, y) = signed.decode().split(',')
-      
-      
+      (r, s) = signed.decode().split(',')
+      print("Sb     : " + str(signed))
+      print("(r,s)  : " + str(r) + "," + str(s))
+            
+      m = str(gx.x) + str(gx.y) + str(gy.x) + str(gy.y)
+      m = m.encode()
+      ecdsa = ECDSA(self.curve)
+      print(ecdsa_publickey)
+      print(ecdsa_publickey.curve)
+      print(self.curve)
+      return ecdsa.verif(ecdsa_publickey, m, r, s)
       
