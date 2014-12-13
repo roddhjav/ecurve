fda12184571dec5f97310b111fcefb68d518bdf5
Alexandre PUJOL <alexandre.pujol.1@etu.univ-amu.fr>
2014-12-13 23:08:14 +0100

    tools: Update the way to read/write the keys

--- tools.py
+++ tools.py
@@ -9,18 +9,9 @@
 # it under the terms of the GNU General Public License.
 #
 
-import base64
 import struct
 from elliptic import EllipticCurve
 from point import Point
-
-__author__ = "Alexandre Pujol and Maxime Chemin"
-__credits__ = ["Alexandre Pujol", "Maxime Chemin"]
-
-__version__= "1.0"
-__license__= "GPL"
-__status__ = "developpement"
-
 
 """ tools
  Some tools
@@ -78,17 +69,14 @@ class key(object):
     Write ELGAMAL, ECDSA or DIFFIEHELLMAN public and private key in a file
     Input :
      - path (string) key path (a public key will have .pub)
-     - algo (string) algo type : ELGAMAL or ECDSA or DiffieHellman
+     - algo (string) algo type : ELGAMAL or ECDSA or DIFFIEHELLMAN
      - curve (EllipticCurve) The elliptic curve used
      - key (int or Point) The key
    """
    @staticmethod
    def writeKey(path, algo, curve, key):
-      f = open(path, "w")
-      
       if algo is not 'ELGAMAL' and algo is not 'ECDSA' and algo is not 'DIFFIEHELLMAN':
          raise Exception('Writing key : Unrecognized algorithm')
-      
       if isinstance(key, Point):
          if algo is 'DIFFIEHELLMAN':
             keytype = 'SHAREDSECRET'
@@ -99,27 +87,24 @@ class key(object):
       else:
          raise Exception('Writing key : Key type error')
       
-      #f.write( str(base64.b64encode( str(curve.p).encode('ascii'))) )
-      
-      f.write("-----BEGIN " + algo + " " + keytype + " KEY-----\n")
-      f.write("p=" + str(curve.p) + "\n")
-      f.write("n=" + str(curve.n) + "\n")
-      f.write("a4=" + str(curve.a4) + "\n")
-      f.write("a6=" + str(curve.a6) + "\n")
-      f.write("r4=" + str(curve.r4) + "\n")
-      f.write("r6=" + str(curve.r6) + "\n")
-      f.write("gx=" + str(curve.gx) + "\n")
-      f.write("gy=" + str(curve.gy) + "\n")
-      f.write("r=" + str(curve.r) + "\n")
-      
-      if isinstance(key, Point):
-         f.write("Kx="+str(key.x)+"\n")
-         f.write("Ky="+str(key.y)+"\n")
-      elif isinstance(key, int) or isinstance(key, long):
-         f.write("K="+str(key)+"\n")
-      
-      f.write("-----END " + algo + " " + keytype + " KEY-----\n")
-      f.close()
+      with open(path, "w") as file:
+         file.write("-----BEGIN " + algo + " " + keytype + " KEY-----\n")
+         file.write("p=" + str(curve.p) + "\n")
+         file.write("n=" + str(curve.n) + "\n")
+         file.write("a4=" + str(curve.a4) + "\n")
+         file.write("a6=" + str(curve.a6) + "\n")
+         file.write("r4=" + str(curve.r4) + "\n")
+         file.write("r6=" + str(curve.r6) + "\n")
+         file.write("gx=" + str(curve.gx) + "\n")
+         file.write("gy=" + str(curve.gy) + "\n")
+         file.write("r=" + str(curve.r) + "\n")
+         if isinstance(key, Point):
+            file.write("Kx=" + str(key.x) + "\n")
+            file.write("Ky=" + str(key.y) + "\n")
+         elif isinstance(key, int) or isinstance(key, long):
+            file.write("K=" + str(key) + "\n")
+         file.write("-----END " + algo + " " + keytype + " KEY-----\n")
+         file.close()
       
    """ readKey
     Read ELGAMAL, ECDSA or DIFFIEHELLMAN public and private key from a file
@@ -131,38 +116,29 @@ class key(object):
    """
    @staticmethod
    def readKey(path):
-      data = {}
-      f = open(path, "r")
-      lines = f.readlines()
-      algo = lines[0].split(' ')[1]    # algo = ELGAMAL or ECDSA or DH
-      keytype = lines[0].split(' ')[2] # keytype = PUBLIC or PRIVATE
-      lines = lines[1:len(lines)-1]
-      
-      for line in lines:
-            (var, val) = line.split('=')
-            data[var] = int(val) 
-      
-      curve = EllipticCurve(  data["p"],
-                              data["n"],
-                              data["a4"],
-                              data["a6"],
-                              data["r4"],
-                              data["r6"],
-                              data["gx"],
-                              data["gy"],
-                              data["r"])
-      
-      if algo != 'ELGAMAL' and algo != 'ECDSA' and algo != 'DIFFIEHELLMAN':
-         raise Exception('Reading key : Unrecognized algorithm')
-
-      if keytype == 'PUBLIC' or keytype == 'SHAREDSECRET':
-         key = Point(curve, data["Kx"], data["Ky"])
-      elif keytype == 'PRIVATE':
-         key = data["K"]
-      else:
-         raise Exception('Reading key : Key type error')
+      with open(path, "r") as file:
+         data = {}
+         lines = file.readlines()
+         algo = lines[0].split(' ')[1]    # algo = ELGAMAL or ECDSA or DH
+         keytype = lines[0].split(' ')[2] # keytype = PUBLIC or PRIVATE
+         lines = lines[1:len(lines)-1]
+         for line in lines:
+               (var, val) = line.split('=')
+               data[var] = int(val) 
          
-      f.close()
+         curve = EllipticCurve(  data["p"], data["n"], data["a4"], data["a6"],
+                                 data["r4"], data["r6"], data["gx"], data["gy"],
+                                 data["r"])
+         
+         if algo != 'ELGAMAL' and algo != 'ECDSA' and algo != 'DIFFIEHELLMAN':
+            raise Exception('Reading key : Unrecognized algorithm')
+         if keytype == 'PUBLIC' or keytype == 'SHAREDSECRET':
+            key = Point(curve, data["Kx"], data["Ky"])
+         elif keytype == 'PRIVATE':
+            key = data["K"]
+         else:
+            raise Exception('Reading key : Key type error')
+         file.close()
       return (curve, key)
  
 class message(object):
@@ -178,7 +154,7 @@ class message(object):
       length = struct.pack('!I',len(msg))
       socket.send(length+msg)
       
-   """ getM
+   """ get
     Get a message from a socket
     Input :
     - socket (Socket)
