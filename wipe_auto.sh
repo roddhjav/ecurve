9f4b2060fba5418028b8ec2101e51dcc6bd05bc5
Alexandre PUJOL <alexandre.pujol.1@etu.univ-amu.fr>
2014-12-22 20:03:51 +0100

    wipe-auto : Add package file

--- wipe_auto.sh
+++ wipe_auto.sh
@@ -1,3 +1,32 @@
 #!/usr/bin/bash
+#
+# Elliptic Curves in python
+# Version 1.0
+# Year 2014
+# Author Alexandre PUJOL <alexandre.pujol.1@etu.univ-amu.fr>
+# Author Maxime CHEMIN <maxime.chemin@etu.univ-amu.fr>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License.
+#
 
+# Byte-compiled / optimized / DLL files
 rm -r __pycache__
+rm *.pyc
+
+# Distribution / packaging
+rm .Python
+rm -r env/
+rm -r build/
+rm -r develop-eggs/
+rm -r dist/
+rm -r downloads/
+rm -r eggs/
+rm -r lib/
+rm -r lib64/
+rm -r parts/
+rm -r sdist/
+rm -r var/
+rm -r *.egg-info/
+rm .installed.cfg
+rm *.egg
