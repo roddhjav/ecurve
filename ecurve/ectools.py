210e2d89cc6ccfc16d9b0f949e4bbe6e24cc4ce9
Alexandre PUJOL <alexandre.pujol.1@etu.univ-amu.fr>
2014-12-22 20:04:22 +0100

    Cosmetic

--- ecurve/ectools.py
+++ ecurve/ectools.py
@@ -1,14 +1,3 @@
-#
-# Elliptic Curves in python
-# Version 1.0
-# Year 2014
-# Author Alexandre PUJOL <alexandre.pujol.1@etu.univ-amu.fr>
-# Author Maxime CHEMIN <maxime.chemin@etu.univ-amu.fr>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License.
-#
-
 """ ectools
  Some tools for elliptic curve
 """      
